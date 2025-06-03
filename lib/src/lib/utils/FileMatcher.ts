73350d33994c30a0a73d1ff1ece75ccb9d122377
Maksim Chertkov <maxch@unl.solutions>
2025-06-03 23:18:19 +0200

    fix endline in filename

--- lib/src/lib/utils/FileMatcher.ts
+++ lib/src/lib/utils/FileMatcher.ts
@@ -9,12 +9,13 @@ export interface TLFileInfo {
 export class FileMatcher {
 
     static combineFileInfo(fileInfoStr: TLFileInfo): string {
-        return fileInfoStr.location + ':' + fileInfoStr.size;
+        // why endline appears in filename?
+        return fileInfoStr.location.replace(/[\r\n]/g, '') + ':' + fileInfoStr.size;
     }
 
     static explodeFileInfo(fileInfoStr: string): TLFileInfo {
         // parse filepath and size
-        const [m, pathAbsolute, size] = fileInfoStr.match(/(.*):([-\d]+)$/) || [null, null, null];
+        const [m, pathAbsolute, size] = fileInfoStr.match(/^(.*):([-\d]+)$/) || [null, null, null];
         if (!m) {
             return null;
         }
