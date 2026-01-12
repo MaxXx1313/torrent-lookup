a505baba58e584fa3d6cc9f5ccd3f6587482db05
Maksim Chertkov <maxch@unl.solutions>
2026-01-12 23:37:20 +0100

    ui4: add data layer

---
+++ electron-ui4/src/electron.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="vite/client" />
+/// <reference types="../../electron-core/core-lib/preload" />
--- electron-ui4/vite.config.ts
+++ electron-ui4/vite.config.ts
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(),
     tailwindcss(),
   ],
   resolve: {
