4c47eb014b6c0ef3c2212b38f8d7a92b37e30c2d
Maksim Chertkov <maxch@unl.solutions>
2026-03-01 22:59:40 +0100

    (lib) fix compile issues

--- lib/src/cli/cli-info-files.ts
+++ lib/src/cli/cli-info-files.ts
@@ -10,8 +10,8 @@ export function cliInfoFiles(options: CliOptions): Promise<any> {
     const info = new Info(options);
     return info.getMapping().then((mapping) => {
         for (let i = 0; i < mapping.length; i++) {
-            console.log('Save:', mapping[i].torrent);
-            console.log('  to:', mapping[i].saveTo);
+            console.log('Save:', mapping[i].torrentLocation);
+            console.log('  to:', mapping[i].saveTo.saveTo);
         }
         console.log('Total:', mapping.length);
     });
